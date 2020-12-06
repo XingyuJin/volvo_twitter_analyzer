450bd0cc0b78cbe4b8977f9e5eff4038f3bc9c82
Xingyu Jin <xingyu.jin21@berkeley.edu>
2020-12-06 20:40:59 +0800

    abstract web crawler class and introduce inheritance

--- dao/twitter_crawler.py
+++ dao/twitter_crawler.py
@@ -14,128 +14,13 @@ from requests.exceptions import RequestException
 from textblob import TextBlob
 
 
-class Twitter:
+class WebCrawler:
     def __init__(self):
-        self.headers = {
-            'accept': '*/*',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'zh-CN,zh;q=0.9',
-            'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
-            'cache-control': 'no-cache',
-            'Cookie': 'personalization_id="v1_sfs7Hmc754bLJF9mLIK2+g=="; tfw_exp=0; guest_id=v1%3A160185784161918610; _ga=GA1.2.634244249.1601857844; dnt=1; ads_prefs="HBISAAA="; kdt=eMkL34tJfk1PiTvHo4dd8ygie7xuOI9l0XERP53M; _twitter_sess=BAh7CiIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCNTyKvZ0AToMY3NyZl9p%250AZCIlYWY2ODgxNDMxYzhjY2Y1YzcxOTc0Nzk4ZjQzNjdmN2U6B2lkIiVjYmZk%250AOWYyOTVhYWUzMWI4Y2FiOGE5M2NiY2M3MWEwNToJdXNlcmwrCQCwFYRCaJ4O--f9314ba074a59c3031a09423f6e8c0c929a0e936; des_opt_in=Y; external_referer=padhuUp37zgIPFFJ%2F%2BwGHSs9bDtnY3elTmanMNkwtWc%3D|0|8e8t2xd8A2w%3D; remember_checked_on=1; auth_token=592d84e00ece02cd847adb9ea8793375a838ec0e; twid=u%3D1053393997744418816; ct0=47ce460bc8877b8c1774afceac09a705bd2ba873f880a8730091f9edd9c83f72f4b7bd1438e18229ecb454ccc123896d7c27bb1e9f8bd16b70c4b9c1f0a8339995c4ed7d269ce95086c32d3f20b8b99f; _gid=GA1.2.624236461.1602141514',
-            'origin': 'https://twitter.com',
-            'pragma': 'no-cache',
-            'referer': 'https://twitter.com/search?q=(%23BlackLivesMatter%20OR%20%23GeorgeFloyd)%20until%3A2020-06-02%20since%3A2020-05-25&src=recent_search_click&f=live',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-site',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
-            'x-csrf-token': '47ce460bc8877b8c1774afceac09a705bd2ba873f880a8730091f9edd9c83f72f4b7bd1438e18229ecb454ccc123896d7c27bb1e9f8bd16b70c4b9c1f0a8339995c4ed7d269ce95086c32d3f20b8b99f',
-            'x-twitter-active-user': 'yes',
-            'x-twitter-auth-type': 'OAuth2Session',
-            'x-twitter-client-language': 'en',
-        }
-
+        self.csv_filename = ""
         self.count = 0
 
-        urllib3.disable_warnings()
-
-        self.current_cursor = ''
-
-        keyword_list_OR = ['volvo', 'volvoc30', 'volvos60', 'volvoxc40bev', 'volvoxc90', 'volvoxc40', 'volvoxc60',
-                           'c30''xc40bev', 'xc40', 'xc90', 's60', 'xc60']
-
-        self.keywords_or = ' OR '.join(keyword_list_OR)
-
-        self.since = '2020-06-01'  # begin
-        self.until = '2020-12-01'  # end
-
-        self.csv_filename = 'volvo0501-1101.csv'
-
     def crawl(self, page):
-        while True:
-            print(f'crawling【{page}】page...')
-            q = quote(f'({self.keywords_or}) lang:en until:{self.until} since:{self.since}', safe='()')
-
-            query = {
-                'include_profile_interstitial_type': 1,
-                'include_blocking': 1,
-                'include_blocked_by': 1,
-                'include_followed_by': 1,
-                'include_want_retweets': 1,
-                'include_mute_edge': 1,
-                'include_can_dm': 1,
-                'include_can_media_tag': 1,
-                'skip_status': 1,
-                'cards_platform': 'Web-12',
-                'include_cards': 1,
-                'include_ext_alt_text': 'true',
-                'include_quote_count': 'true',
-                'include_reply_count': 1,
-                'tweet_mode': 'extended',
-                'include_entities': 'true',
-                'include_user_entities': 'true',
-                'include_ext_media_color': 'true',
-                'include_ext_media_availability': 'true',
-                'send_error_codes': 'true',
-                'simple_quoted_tweet': 'true',
-                'tweet_search_mode': 'live',
-                'count': 20,
-                'query_source': 'recent_search_click',
-                'pc': 1,
-                'spelling_corrections': 1,
-                'ext': 'mediaStats,highlightedLabel'
-            }
-
-            if page == 1:
-                url = 'https://api.twitter.com/2/search/adaptive.json?' + urlencode(query) + f'&q={q}'
-            else:
-                url = 'https://api.twitter.com/2/search/adaptive.json?' + urlencode(
-                    query) + f'&q={q}&cursor={self.current_cursor}'
-
-            try:
-                # print(url)
-                response = requests.get(url, headers=self.headers, verify=False, timeout=6)
-
-                if response.status_code == 200:
-                    datas = response.json()
-                    tweets = datas.get('globalObjects').get('tweets')
-                    users = datas.get('globalObjects').get('users')
-
-                    if tweets:
-                        self.parse_and_save_detail(tweets, users)
-
-                        try:
-                            if page == 1:
-                                cursor_value = \
-                                    datas.get('timeline').get('instructions')[0].get('addEntries').get('entries')[
-                                        -1].get(
-                                        'content').get('operation').get('cursor').get('value')
-                            else:
-                                cursor_value = datas.get('timeline').get('instructions')[-1].get('replaceEntry').get(
-                                    'entry').get('content').get('operation').get('cursor').get('value')
-                        except AttributeError:
-                            return None
-
-                        else:
-                            if cursor_value:
-                                cursor = quote(cursor_value)
-                                return cursor
-                            else:
-                                return None
-
-                    else:
-                        return None
-
-                else:
-                    time.sleep(1)
-                    continue
-
-            except RequestException as _:
-                time.sleep(1)
-                continue
-            except json.decoder.JSONDecodeError as _:
-                time.sleep(1)
-                continue
+        raise AssertionError("This is an abstract class and should not be used for crawling")
 
     @staticmethod
     def init_detail(tweet_id, user_id, content, retweet_id):
@@ -151,7 +36,8 @@ class Twitter:
             'Tweet_time': ' ',
             'Tweet_location': ' ',
             'Tweet_country_code': ' ',
-            'Tweet_content': re.sub(r'https?://.*?\s', ' ', content.get('full_text').replace('\n', '\t') + ' ').strip(),
+            'Tweet_content': re.sub(r'https?://.*?\s', ' ',
+                                    content.get('full_text').replace('\n', '\t') + ' ').strip(),
             'Tweet_contain_links': ', '.join(
                 re.findall(r'(https?://.*?)\s', content.get('full_text').replace('\n', '\t') + ' ')),
             'Tweet_image_urls': ' ',
@@ -295,6 +181,247 @@ class Twitter:
                     break
             else:
                 break
+
+
+class Twitter(WebCrawler):
+    def __init__(self):
+        super().__init__()
+        self.headers = {
+            'accept': '*/*',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
+            'cache-control': 'no-cache',
+            'Cookie': 'personalization_id="v1_sfs7Hmc754bLJF9mLIK2+g=="; tfw_exp=0; guest_id=v1%3A160185784161918610; _ga=GA1.2.634244249.1601857844; dnt=1; ads_prefs="HBISAAA="; kdt=eMkL34tJfk1PiTvHo4dd8ygie7xuOI9l0XERP53M; _twitter_sess=BAh7CiIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCNTyKvZ0AToMY3NyZl9p%250AZCIlYWY2ODgxNDMxYzhjY2Y1YzcxOTc0Nzk4ZjQzNjdmN2U6B2lkIiVjYmZk%250AOWYyOTVhYWUzMWI4Y2FiOGE5M2NiY2M3MWEwNToJdXNlcmwrCQCwFYRCaJ4O--f9314ba074a59c3031a09423f6e8c0c929a0e936; des_opt_in=Y; external_referer=padhuUp37zgIPFFJ%2F%2BwGHSs9bDtnY3elTmanMNkwtWc%3D|0|8e8t2xd8A2w%3D; remember_checked_on=1; auth_token=592d84e00ece02cd847adb9ea8793375a838ec0e; twid=u%3D1053393997744418816; ct0=47ce460bc8877b8c1774afceac09a705bd2ba873f880a8730091f9edd9c83f72f4b7bd1438e18229ecb454ccc123896d7c27bb1e9f8bd16b70c4b9c1f0a8339995c4ed7d269ce95086c32d3f20b8b99f; _gid=GA1.2.624236461.1602141514',
+            'origin': 'https://twitter.com',
+            'pragma': 'no-cache',
+            'referer': 'https://twitter.com/search?q=(%23BlackLivesMatter%20OR%20%23GeorgeFloyd)%20until%3A2020-06-02%20since%3A2020-05-25&src=recent_search_click&f=live',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+            'x-csrf-token': '47ce460bc8877b8c1774afceac09a705bd2ba873f880a8730091f9edd9c83f72f4b7bd1438e18229ecb454ccc123896d7c27bb1e9f8bd16b70c4b9c1f0a8339995c4ed7d269ce95086c32d3f20b8b99f',
+            'x-twitter-active-user': 'yes',
+            'x-twitter-auth-type': 'OAuth2Session',
+            'x-twitter-client-language': 'en',
+        }
+
+        urllib3.disable_warnings()
+
+        self.current_cursor = ''
+
+        keyword_list_OR = ['volvo', 'volvoc30', 'volvos60', 'volvoxc40bev', 'volvoxc90', 'volvoxc40', 'volvoxc60',
+                           'c30''xc40bev', 'xc40', 'xc90', 's60', 'xc60']
+
+        self.keywords_or = ' OR '.join(keyword_list_OR)
+
+        self.since = '2020-06-01'  # begin
+        self.until = '2020-12-01'  # end
+
+        self.csv_filename = 'volvo0501-1101.csv'
+
+    def crawl(self, page):
+        while True:
+            q = quote(f'({self.keywords_or}) lang:en until:{self.until} since:{self.since}', safe='()')
+
+            query = {
+                'include_profile_interstitial_type': 1,
+                'include_blocking': 1,
+                'include_blocked_by': 1,
+                'include_followed_by': 1,
+                'include_want_retweets': 1,
+                'include_mute_edge': 1,
+                'include_can_dm': 1,
+                'include_can_media_tag': 1,
+                'skip_status': 1,
+                'cards_platform': 'Web-12',
+                'include_cards': 1,
+                'include_ext_alt_text': 'true',
+                'include_quote_count': 'true',
+                'include_reply_count': 1,
+                'tweet_mode': 'extended',
+                'include_entities': 'true',
+                'include_user_entities': 'true',
+                'include_ext_media_color': 'true',
+                'include_ext_media_availability': 'true',
+                'send_error_codes': 'true',
+                'simple_quoted_tweet': 'true',
+                'tweet_search_mode': 'live',
+                'count': 20,
+                'query_source': 'recent_search_click',
+                'pc': 1,
+                'spelling_corrections': 1,
+                'ext': 'mediaStats,highlightedLabel'
+            }
+
+            if page == 1:
+                url = 'https://api.twitter.com/2/search/adaptive.json?' + urlencode(query) + f'&q={q}'
+            else:
+                url = 'https://api.twitter.com/2/search/adaptive.json?' + urlencode(
+                    query) + f'&q={q}&cursor={self.current_cursor}'
+
+            try:
+                # print(url)
+                response = requests.get(url, headers=self.headers, verify=False, timeout=6)
+
+                if response.status_code == 200:
+                    datas = response.json()
+                    tweets = datas.get('globalObjects').get('tweets')
+                    users = datas.get('globalObjects').get('users')
+
+                    if tweets:
+                        self.parse_and_save_detail(tweets, users)
+
+                        try:
+                            if page == 1:
+                                cursor_value = \
+                                    datas.get('timeline').get('instructions')[0].get('addEntries').get('entries')[
+                                        -1].get('content').get('operation').get('cursor').get('value')
+                            else:
+                                cursor_value = datas.get('timeline').get('instructions')[-1].get('replaceEntry').get(
+                                    'entry').get('content').get('operation').get('cursor').get('value')
+                        except AttributeError:
+                            return None
+
+                        else:
+                            if cursor_value:
+                                cursor = quote(cursor_value)
+                                return cursor
+                            else:
+                                return None
+
+                    else:
+                        return None
+
+                else:
+                    time.sleep(1)
+                    continue
+
+            except RequestException as _:
+                time.sleep(1)
+                continue
+            except json.decoder.JSONDecodeError as _:
+                time.sleep(1)
+                continue
+
+
+class VolvoAccount(WebCrawler):
+    def __init__(self):
+        super().__init__()
+        self.headers = {
+            'authority': 'twitter.com',
+            'pragma': 'no-cache',
+            'cache-control': 'no-cache',
+            'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
+            'x-twitter-client-language': 'en',
+            'x-csrf-token': '47ce460bc8877b8c1774afceac09a705bd2ba873f880a8730091f9edd9c83f72f4b7bd1438e18229ecb454ccc123896d7c27bb1e9f8bd16b70c4b9c1f0a8339995c4ed7d269ce95086c32d3f20b8b99f',
+            # 需替换
+            'x-twitter-auth-type': 'OAuth2Session',
+            'x-twitter-active-user': 'yes',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+            'accept': '*/*',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-dest': 'empty',
+            # 'referer': 'https://twitter.com/search?q=%23PokemonforHK&src=recent_search_click&f=live',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'cookie': 'personalization_id="v1_sfs7Hmc754bLJF9mLIK2+g=="; tfw_exp=0; guest_id=v1%3A160185784161918610; _ga=GA1.2.634244249.1601857844; dnt=1; ads_prefs="HBISAAA="; kdt=eMkL34tJfk1PiTvHo4dd8ygie7xuOI9l0XERP53M; _twitter_sess=BAh7CiIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCNTyKvZ0AToMY3NyZl9p%250AZCIlYWY2ODgxNDMxYzhjY2Y1YzcxOTc0Nzk4ZjQzNjdmN2U6B2lkIiVjYmZk%250AOWYyOTVhYWUzMWI4Y2FiOGE5M2NiY2M3MWEwNToJdXNlcmwrCQCwFYRCaJ4O--f9314ba074a59c3031a09423f6e8c0c929a0e936; des_opt_in=Y; external_referer=padhuUp37zgIPFFJ%2F%2BwGHSs9bDtnY3elTmanMNkwtWc%3D|0|8e8t2xd8A2w%3D; remember_checked_on=1; auth_token=592d84e00ece02cd847adb9ea8793375a838ec0e; twid=u%3D1053393997744418816; ct0=47ce460bc8877b8c1774afceac09a705bd2ba873f880a8730091f9edd9c83f72f4b7bd1438e18229ecb454ccc123896d7c27bb1e9f8bd16b70c4b9c1f0a8339995c4ed7d269ce95086c32d3f20b8b99f; _gid=GA1.2.624236461.1602141514'
+        }
+
+        self.count = 0
+
+        urllib3.disable_warnings()
+
+        self.current_cursor = ''
+
+        self.csv_filename = 'VolvoCarUSA.csv'
+        # self.keyword = 'volvo'
+        self.since = '2019-11-01'
+        self.until = '2020-11-01'
+        self.account = 'VolvoCarUSA'
+
+    def crawl(self, page):
+        while True:
+            q = quote(f'(from:{self.account}) lang:en until:{self.until} since:{self.since}', safe='()')
+
+            query = {
+                'include_profile_interstitial_type': 1,
+                'include_blocking': 1,
+                'include_blocked_by': 1,
+                'include_followed_by': 1,
+                'include_want_retweets': 1,
+                'include_mute_edge': 1,
+                'include_can_dm': 1,
+                'include_can_media_tag': 1,
+                'skip_status': 1,
+                'cards_platform': 'Web-12',
+                'include_cards': 1,
+                'include_ext_alt_text': 'true',
+                'include_quote_count': 'true',
+                'include_reply_count': 1,
+                'tweet_mode': 'extended',
+                'include_entities': 'true',
+                'include_user_entities': 'true',
+                'include_ext_media_color': 'true',
+                'include_ext_media_availability': 'true',
+                'send_error_codes': 'true',
+                'simple_quoted_tweet': 'true',
+                'tweet_search_mode': 'live',
+                'count': 20,
+                'query_source': 'recent_search_click',
+                'pc': 1,
+                'spelling_corrections': 1,
+                'ext': 'mediaStats,highlightedLabel'
+            }
+
+            if page == 1:
+                url = 'https://api.twitter.com/2/search/adaptive.json?' + urlencode(query) + f'&q={q}'
+            else:
+                url = 'https://api.twitter.com/2/search/adaptive.json?' + urlencode(
+                    query) + f'&q={q}&cursor={self.current_cursor}'
+
+            try:
+                # print(url)
+                response = requests.get(url, headers=self.headers, verify=False, timeout=6)
+
+                if response.status_code == 200:
+                    datas = response.json()
+                    tweets = datas.get('globalObjects').get('tweets')
+                    users = datas.get('globalObjects').get('users')
+
+                    if tweets:
+                        self.parse_and_save_detail(tweets, users)
+
+                        try:
+                            if page == 1:
+                                cursor_value = \
+                                    datas.get('timeline').get('instructions')[0].get('addEntries').get('entries')[
+                                        -1].get('content').get('operation').get('cursor').get('value')
+                            else:
+                                cursor_value = datas.get('timeline').get('instructions')[-1].get('replaceEntry').get(
+                                    'entry').get('content').get('operation').get('cursor').get('value')
+                        except AttributeError:
+                            return None
+
+                        else:
+                            if cursor_value:
+                                cursor = quote(cursor_value)
+                                return cursor
+                            else:
+                                return None
+
+                    else:
+                        return None
+
+                else:
+                    time.sleep(1)
+                    continue
+
+            except RequestException as _:
+                time.sleep(1)
+                continue
+            except json.decoder.JSONDecodeError as _:
+                time.sleep(1)
+                continue
 
 
 def preprocess(csv_name):
